cf91768206efa5e8652c41c1453ddb0c7c1dd716
Artem Otliaguzov <temaotl0992@gmail.com>
2024-01-25 15:06:49 +0100

    start

---
+++ petblog/src/main/java/org/example/petblog/PetblogApplication.java
@@ -0,0 +1,13 @@
+package org.example.petblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetblogApplication.class, args);
+    }
+
+}
---
+++ petblog/src/test/java/org/example/petblog/PetblogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.petblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
